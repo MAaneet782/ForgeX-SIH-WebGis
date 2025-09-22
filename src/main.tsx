fb48a2c784144e081243023a43355edf4ee44403
[dyad] <maneetsahej782@gmail.com>
2025-09-22 23:58:25 +0530

    [dyad] Temporarily removed React.StrictMode from src/main.tsx to troubleshoot the React.Children.only error. - wrote 1 file(s)

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,12 @@ import App from "./App.tsx";
 import "./globals.css";
 import "leaflet/dist/leaflet.css";
 import { ThemeProvider } from "./components/theme-provider";
-import React from "react"; // Import React for StrictMode
+// Removed: import React from "react"; // No longer needed if StrictMode is removed
 
 createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
+  // Removed: <React.StrictMode>
     <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
       <App />
     </ThemeProvider>
-  </React.StrictMode>,
+  // Removed: </React.StrictMode>,
 );
