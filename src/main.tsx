430e6bb1fcfb5b2c695e41f35b896c942977ec07
[dyad] <maneetsahej782@gmail.com>
2025-09-23 00:06:49 +0530

    [dyad] Temporarily removed React.StrictMode and ThemeProvider from main.tsx to diagnose the persistent React.Children.only error and restore app preview. - wrote 1 file(s)

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,13 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./globals.css";
 import "leaflet/dist/leaflet.css";
-import { ThemeProvider } from "./components/theme-provider";
-import React from "react"; // Import React for StrictMode
+// import { ThemeProvider } from "./components/theme-provider"; // Temporarily removed
+import React from "react"; // Still needed for JSX, but StrictMode is removed
 
 createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+  // <React.StrictMode> // Temporarily removed
+    // <ThemeProvider attribute="class" defaultTheme="system" enableSystem> // Temporarily removed
       <App />
-    </ThemeProvider>
-  </React.StrictMode>,
+    // </ThemeProvider> // Temporarily removed
+  // </React.StrictMode>, // Temporarily removed
 );
