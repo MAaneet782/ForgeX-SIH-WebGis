2d5ccc751724000ac00acab98e14fd50eaf79d22
[dyad] <maneetsahej782@gmail.com>
2025-09-23 00:23:17 +0530

    [dyad] Refactored App.tsx to make Routes a direct child of BrowserRouter and moved Toaster to main.tsx outside of App to resolve the React.Children.only error. - wrote 2 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-// import { Toaster } from "sonner"; // Temporarily removed Toaster
+// import { Toaster } from "sonner"; // Toaster will be moved to main.tsx
 
 import LandingPage from "./pages/Landing";
 import Atlas from "./pages/Atlas";
@@ -11,17 +11,13 @@ import NotFound from "./pages/NotFound";
 const App = () => {
   return (
     <BrowserRouter>
-      {/* BrowserRouter expects a single child. We'll use a React.Fragment to wrap Routes. */}
-      <React.Fragment>
-        {/* <Toaster richColors position="top-right" /> */}
-        <Routes>
-          <Route path="/" element={<LandingPage />} />
-          <Route path="/atlas" element={<Atlas />} />
-          <Route path="/atlas/claim/:claimId" element={<ClaimDetail />} />
-          <Route path="/atlas/analytics" element={<Analytics />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </React.Fragment>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/atlas" element={<Atlas />} />
+        <Route path="/atlas/claim/:claimId" element={<ClaimDetail />} />
+        <Route path="/atlas/analytics" element={<Analytics />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
     </BrowserRouter>
   );
 };
--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,13 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.tsx"; // Re-importing App
+import App from "./App.tsx";
 import "./globals.css";
 import "leaflet/dist/leaflet.css";
-
-// import { ThemeProvider } from "./components/theme-provider"; // Still commented out
-// import React from "react"; // Still needed for JSX, but StrictMode is removed
+import { Toaster } from "sonner"; // Re-importing Toaster
 
 createRoot(document.getElementById("root")!).render(
-  <>
+  <React.Fragment>
+    <Toaster richColors position="top-right" />
     <App />
-  </>
+  </React.Fragment>
 );
