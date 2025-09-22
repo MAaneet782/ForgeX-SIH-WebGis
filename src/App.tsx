9b87c3055422bd6d192d29599678aef6caa22a41
[dyad] <maneetsahej782@gmail.com>
2025-09-22 23:53:18 +0530

    [dyad] Temporarily simplified App.tsx to isolate the React.Children.only error. - wrote 1 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,11 @@
-import { TooltipProvider } from "@/components/ui/tooltip";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Landing from "./pages/Landing";
-import Atlas from "./pages/Atlas";
-import NotFound from "./pages/NotFound";
-import ClaimDetail from "./pages/ClaimDetail";
-import Analytics from "./pages/Analytics";
-import { Toaster as Sonner } from "@/components/ui/sonner"; // Import Sonner here
-import React from "react"; // Import React for Fragment
+import React from "react";
 
-const queryClient = new QueryClient();
-
-const App = () => (
-  <QueryClientProvider client={queryClient}>
-    <TooltipProvider>
-      <BrowserRouter>
-        <React.Fragment> {/* Wrap Sonner and Routes in a Fragment */}
-          <Sonner /> {/* Render Sonner here */}
-          <Routes>
-            <Route path="/" element={<Landing />} />
-            <Route path="/atlas" element={<Atlas />} />
-            <Route path="/atlas/claim/:claimId" element={<ClaimDetail />} />
-            <Route path="/atlas/analytics" element={<Analytics />} />
-            {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </React.Fragment>
-      </BrowserRouter>
-    </TooltipProvider>
-  </QueryClientProvider>
-);
+const App = () => {
+  return (
+    <div>
+      Hello from App!
+    </div>
+  );
+};
 
 export default App;
