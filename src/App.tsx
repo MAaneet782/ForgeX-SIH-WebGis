39fbabac552131ec54bac3595c4de9cca0a88c7d
[dyad] <maneetsahej782@gmail.com>
2025-09-23 00:27:45 +0530

    [dyad] Fixed the router context error by re-implementing the full routing structure in App.tsx. - wrote 1 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,26 @@
 import React from "react";
-import LandingPage from "./pages/Landing";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Toaster } from "sonner";
+
+import LandingPage from "./pages/Landing";
+import Atlas from "./pages/Atlas";
+import ClaimDetail from "./pages/ClaimDetail";
+import Analytics from "./pages/Analytics";
+import NotFound from "./pages/NotFound";
 
 const App = () => {
   return (
     <>
       <Toaster richColors position="top-right" />
-      <LandingPage />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/atlas" element={<Atlas />} />
+          <Route path="/atlas/claim/:claimId" element={<ClaimDetail />} />
+          <Route path="/atlas/analytics" element={<Analytics />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 };
