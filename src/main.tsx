fb1b518b6b4ef41f2b3c776a1391c648950fccc1
[dyad] <maneetsahej782@gmail.com>
2025-09-22 23:44:30 +0530

    [dyad] Removed the unused shadcn/ui Toaster component and its related files to resolve potential conflicts and streamline the toast notification system, and reminded the user to set Supabase environment variables. - wrote 1 file(s), deleted 4 file(s)

--- src/main.tsx
+++ src/main.tsx
@@ -3,13 +3,11 @@ import App from "./App.tsx";
 import "./globals.css";
 import "leaflet/dist/leaflet.css";
 import { ThemeProvider } from "./components/theme-provider";
-import { Toaster } from "@/components/ui/toaster"; // Import Toaster
 import { Toaster as Sonner } from "@/components/ui/sonner"; // Import Sonner
 
 createRoot(document.getElementById("root")!).render(
   <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
     <App />
-    <Toaster /> {/* Render Toaster here */}
     <Sonner /> {/* Render Sonner here */}
   </ThemeProvider>,
 );
