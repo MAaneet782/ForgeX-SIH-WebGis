53ec8aa8601dc6b207e767fbc54f5b9009a230ac
[dyad] <maneetsahej782@gmail.com>
2025-09-22 23:47:36 +0530

    [dyad] Fixed the React.Children.only error by wrapping Sonner and Routes in a React.Fragment within BrowserRouter. - wrote 1 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import NotFound from "./pages/NotFound";
 import ClaimDetail from "./pages/ClaimDetail";
 import Analytics from "./pages/Analytics";
 import { Toaster as Sonner } from "@/components/ui/sonner"; // Import Sonner here
+import React from "react"; // Import React for Fragment
 
 const queryClient = new QueryClient();
 
@@ -14,15 +15,17 @@ const App = () => (
   <QueryClientProvider client={queryClient}>
     <TooltipProvider>
       <BrowserRouter>
-        <Sonner /> {/* Render Sonner here, inside BrowserRouter */}
-        <Routes>
-          <Route path="/" element={<Landing />} />
-          <Route path="/atlas" element={<Atlas />} />
-          <Route path="/atlas/claim/:claimId" element={<ClaimDetail />} />
-          <Route path="/atlas/analytics" element={<Analytics />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-          <Route path="*" element={<NotFound />} />
-        </Routes>
+        <React.Fragment> {/* Wrap Sonner and Routes in a Fragment */}
+          <Sonner /> {/* Render Sonner here */}
+          <Routes>
+            <Route path="/" element={<Landing />} />
+            <Route path="/atlas" element={<Atlas />} />
+            <Route path="/atlas/claim/:claimId" element={<ClaimDetail />} />
+            <Route path="/atlas/analytics" element={<Analytics />} />
+            {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </React.Fragment>
       </BrowserRouter>
     </TooltipProvider>
   </QueryClientProvider>
