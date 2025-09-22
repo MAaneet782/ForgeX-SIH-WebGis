b61d5be96a7723379a257af0c728d1a50aa086a2
[dyad] <maneetsahej782@gmail.com>
2025-09-22 23:22:03 +0530

    [dyad] Fixed a React.Children.only error by correcting the implementation of the Toaster component to properly use ToastProvider and ToastViewport. - wrote 1 file(s)

--- src/components/ui/toaster.tsx
+++ src/components/ui/toaster.tsx
@@ -1,4 +1,5 @@
-import { useToast } from "@/hooks/use-toast";
+"use client"
+
 import {
   Toast,
   ToastClose,
@@ -6,28 +7,27 @@ import {
   ToastProvider,
   ToastTitle,
   ToastViewport,
-} from "@/components/ui/toast";
+} from "@/components/ui/toast"
+import { useToast } from "@/hooks/use-toast"
 
 export function Toaster() {
-  const { toasts } = useToast();
+  const { toasts } = useToast()
 
   return (
     <ToastProvider>
+      <ToastViewport />
       {toasts.map(function ({ id, title, description, action, ...props }) {
         return (
           <Toast key={id} {...props}>
-            <div className="grid gap-1">
-              {title && <ToastTitle>{title}</ToastTitle>}
-              {description && (
-                <ToastDescription>{description}</ToastDescription>
-              )}
-            </div>
+            {title && <ToastTitle>{title}</ToastTitle>}
+            {description && (
+              <ToastDescription>{description}</ToastDescription>
+            )}
             {action}
             <ToastClose />
           </Toast>
-        );
+        )
       })}
-      <ToastViewport />
     </ToastProvider>
-  );
+  )
 }
