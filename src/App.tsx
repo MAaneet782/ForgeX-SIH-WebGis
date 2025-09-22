994d439c45aa7e3e8d5428ca5601b40a54e6f449
[dyad] <maneetsahej782@gmail.com>
2025-09-23 00:13:07 +0530

    [dyad] Removed outer fragment from App.tsx and re-enabled all routes to fix rendering error. - wrote 1 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,25 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-// import { Toaster } from "sonner"; // Temporarily removed to diagnose rendering issue
+import { Toaster } from "sonner"; // Re-enabling Toaster
 
 import LandingPage from "./pages/Landing";
-// import Atlas from "./pages/Atlas"; // Temporarily removed
-// import ClaimDetail from "./pages/ClaimDetail"; // Temporarily removed
-// import Analytics from "./pages/Analytics"; // Temporarily removed
+import Atlas from "./pages/Atlas"; // Re-enabled
+import ClaimDetail from "./pages/ClaimDetail"; // Re-enabled
+import Analytics from "./pages/Analytics"; // Re-enabled
 import NotFound from "./pages/NotFound";
 
 const App = () => {
   return (
-    <>
-      {/* <Toaster richColors position="top-right" /> */}
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<LandingPage />} />
-          {/* Temporarily removed Supabase-dependent routes */}
-          {/* <Route path="/atlas" element={<Atlas />} /> */}
-          {/* <Route path="/atlas/claim/:claimId" element={<ClaimDetail />} /> */}
-          {/* <Route path="/atlas/analytics" element={<Analytics />} /> */}
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
-    </>
+    <BrowserRouter> {/* BrowserRouter is now the direct return */}
+      <Toaster richColors position="top-right" /> {/* Toaster moved inside BrowserRouter */}
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/atlas" element={<Atlas />} />
+        <Route path="/atlas/claim/:claimId" element={<ClaimDetail />} />
+        <Route path="/atlas/analytics" element={<Analytics />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
