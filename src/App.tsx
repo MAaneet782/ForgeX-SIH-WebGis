9b5641072e97d6818fd497dfb735df7f11116958
[dyad] <maneetsahej782@gmail.com>
2025-09-22 23:55:46 +0530

    [dyad] Restored the full application by re-implementing routing and the Toaster component in App.tsx. - wrote 1 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,25 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Toaster } from "sonner"; // Import Toaster for notifications
+
+import LandingPage from "./pages/Landing";
+import Atlas from "./pages/Atlas";
+import ClaimDetail from "./pages/ClaimDetail";
+import Analytics from "./pages/Analytics";
+import NotFound from "./pages/NotFound";
 
 const App = () => {
   return (
-    <div>
-      Hello from App!
-    </div>
+    <BrowserRouter>
+      <Toaster richColors position="top-right" /> {/* Toaster for notifications */}
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/atlas" element={<Atlas />} />
+        <Route path="/atlas/claim/:claimId" element={<ClaimDetail />} />
+        <Route path="/atlas/analytics" element={<Analytics />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
