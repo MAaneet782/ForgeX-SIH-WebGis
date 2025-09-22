054292b0714eb2f2146af35c24500c835f0bd316
[dyad] <maneetsahej782@gmail.com>
2025-09-23 00:12:18 +0530

    [dyad] Temporarily removed Supabase-dependent routes from App.tsx to diagnose the rendering issue. - wrote 1 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -3,21 +3,22 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 // import { Toaster } from "sonner"; // Temporarily removed to diagnose rendering issue
 
 import LandingPage from "./pages/Landing";
-import Atlas from "./pages/Atlas";
-import ClaimDetail from "./pages/ClaimDetail";
-import Analytics from "./pages/Analytics";
+// import Atlas from "./pages/Atlas"; // Temporarily removed
+// import ClaimDetail from "./pages/ClaimDetail"; // Temporarily removed
+// import Analytics from "./pages/Analytics"; // Temporarily removed
 import NotFound from "./pages/NotFound";
 
 const App = () => {
   return (
     <>
-      {/* <Toaster richColors position="top-right" /> */} {/* Toaster temporarily removed */}
+      {/* <Toaster richColors position="top-right" /> */}
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<LandingPage />} />
-          <Route path="/atlas" element={<Atlas />} />
-          <Route path="/atlas/claim/:claimId" element={<ClaimDetail />} />
-          <Route path="/atlas/analytics" element={<Analytics />} />
+          {/* Temporarily removed Supabase-dependent routes */}
+          {/* <Route path="/atlas" element={<Atlas />} /> */}
+          {/* <Route path="/atlas/claim/:claimId" element={<ClaimDetail />} /> */}
+          {/* <Route path="/atlas/analytics" element={<Analytics />} /> */}
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
