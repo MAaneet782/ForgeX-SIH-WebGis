93ffcf9505c7b17fe33230595f4b06c7871ac941
[dyad] <maneetsahej782@gmail.com>
2025-09-23 00:26:03 +0530

    [dyad] Stripped the app down to its minimum by removing the router to isolate and fix the persistent render error. - wrote 2 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,13 @@
 import React from "react";
-import { HashRouter, Routes, Route } from "react-router-dom";
 import LandingPage from "./pages/Landing";
-import Atlas from "./pages/Atlas";
-import ClaimDetail from "./pages/ClaimDetail";
-import Analytics from "./pages/Analytics";
-import NotFound from "./pages/NotFound";
+import { Toaster } from "sonner";
 
 const App = () => {
   return (
-    <HashRouter>
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/atlas" element={<Atlas />} />
-        <Route path="/atlas/claim/:claimId" element={<ClaimDetail />} />
-        <Route path="/atlas/analytics" element={<Analytics />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </HashRouter>
+    <>
+      <Toaster richColors position="top-right" />
+      <LandingPage />
+    </>
   );
 };
 
--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,9 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./globals.css";
 import "leaflet/dist/leaflet.css";
-import { Toaster } from "sonner";
 
 createRoot(document.getElementById("root")!).render(
-  <React.Fragment>
-    <Toaster richColors position="top-right" />
+  <React.StrictMode>
     <App />
-  </React.Fragment>
+  </React.StrictMode>
 );
