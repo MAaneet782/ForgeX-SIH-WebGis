fa8db5476fa7e5249cdf8affbeec9b4e4287983c
[dyad] <maneetsahej782@gmail.com>
2025-09-23 00:15:13 +0530

    [dyad] Fixed the React.Children.only error by wrapping Toaster and Routes in a fragment within BrowserRouter. - wrote 1 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,27 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { Toaster } from "sonner"; // Re-enabling Toaster
+import { Toaster } from "sonner";
 
 import LandingPage from "./pages/Landing";
-import Atlas from "./pages/Atlas"; // Re-enabled
-import ClaimDetail from "./pages/ClaimDetail"; // Re-enabled
-import Analytics from "./pages/Analytics"; // Re-enabled
+import Atlas from "./pages/Atlas";
+import ClaimDetail from "./pages/ClaimDetail";
+import Analytics from "./pages/Analytics";
 import NotFound from "./pages/NotFound";
 
 const App = () => {
   return (
-    <BrowserRouter> {/* BrowserRouter is now the direct return */}
-      <Toaster richColors position="top-right" /> {/* Toaster moved inside BrowserRouter */}
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/atlas" element={<Atlas />} />
-        <Route path="/atlas/claim/:claimId" element={<ClaimDetail />} />
-        <Route path="/atlas/analytics" element={<Analytics />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
+    <BrowserRouter>
+      {/* BrowserRouter expects a single child, so we wrap Toaster and Routes in a fragment */}
+      <React.Fragment>
+        <Toaster richColors position="top-right" />
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/atlas" element={<Atlas />} />
+          <Route path="/atlas/claim/:claimId" element={<ClaimDetail />} />
+          <Route path="/atlas/analytics" element={<Analytics />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </React.Fragment>
     </BrowserRouter>
   );
 };
