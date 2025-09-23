259894a4edfd2c551ee2c9a16ab5f2df3d22b70e
[dyad] <maneetsahej782@gmail.com>
2025-09-23 11:57:15 +0530

    [dyad] Fixed database error and populated with sample data - wrote 1 file(s), executed 2 SQL queries

---
+++ supabase/migrations/0003_updating_the_soil_type_enum_to_include_new_values_.sql
@@ -0,0 +1,5 @@
+ALTER TYPE public.soil_type ADD VALUE IF NOT EXISTS 'Red';
+ALTER TYPE public.soil_type ADD VALUE IF NOT EXISTS 'Laterite';
+ALTER TYPE public.soil_type ADD VALUE IF NOT EXISTS 'Alluvial';
+ALTER TYPE public.soil_type ADD VALUE IF NOT EXISTS 'Black';
+ALTER TYPE public.soil_type ADD VALUE IF NOT EXISTS 'Mountain';
